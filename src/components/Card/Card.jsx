dc3d5f3ccbeafab0aa329fcafbf7f812a16838fa
Jude <lovespictures2@gmail.com>
2022-06-16 21:07:27 +0100

    Updated Project cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import img1 from "../img/1.jpg";
 // import github from "../img/github.svg";
 // import netlify from "../img/netlify.csv";
 // refactored
-import Icons from "../img/Icons"
+import Icons from "../img/Icons";
 
 import "../Section/Section.css";
 import "../Card/Card.css";
@@ -18,26 +18,28 @@ make small changes  - check live server
 image https://create-react-app.dev/docs/adding-images-fonts-and-files/
 */
 // prop value inside curlie brackets
-export const Card = ({project}) => {
+export const Card = ({ project }) => {
   return (
     <div className="Card">
-      <h3 className="Card__header ">{project.title}</h3>
       <img className="Card__image" src={project.img} alt="#" />
+
+      <h3 className="Card__header ">{project.title}</h3>
       {/* container for the icons */}
       <ul className="Card__ul">
         <a className="Card__icon" target="_blank" href={project.linkGithub}>
           <img src={Icons.iconGithub} alt="#" />
         </a>
         <a className="Card__icon" target="_blank" href={project.linkNetlify}>
-
           <img src={Icons.iconNetlify} width="30" alt="#" />
           {/* To do - assing a name to the icons */}
           <p>{project.iconName}</p>
         </a>
       </ul>
       {/* roll over project name for more information */}
-    <p className="Card__description"><strong>Project Description:</strong> {project.description}
-             </p>
+      <p className="Card__description">
+        {/* <strong>Project Description:</strong> {project.description} */}
+        {project.description}
+      </p>
     </div>
   );
 };
